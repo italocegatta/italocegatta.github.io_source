39fc7a3f3b1fe1cefd7de132f2c28aa925820365
italocegatta <italocegatta@gmail.com>
2017-09-30 17:32:02 -0300

    clean draft

--- content/post/joyplot.R
+++ content/post/joyplot.R
@@ -1,45 +1,21 @@
----
-title: Os gráficos que explicam nossos dados (joyplot)
-date: 2017-08-20
-author: Ítalo Cegatta
-draft: true
-tags:
-categories:
-  - Data Science
-thumbnailImage: http://i.imgur.com/1gZcM9L.png
-coverImage: http://i.imgur.com/d1eBDPc.png
-coverCaption:
-thumbnailImagePosition: top
-metaAlignment: center
-coverMeta: out
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(
-  warning = FALSE, message = FALSE,
-  fig.width = 10, fig.height = 8, dpi = 400
-)
-```
-
-```{r}
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(readr, dplyr, RcppRoll, lubridate, stringr, ggplot2, ggjoy, ggthemes)
-```
 
 
-```{r}
+
+
 clima <- read_csv2("https://raw.githubusercontent.com/italocegatta/italocegatta.github.io_source/master/content/dados/posto_esalq.csv") %>% 
   mutate(
-  data = dmy(data),
-  ano = year(data),
-  decada_label = cut(ano, breaks = seq(1910, 2020, by = 10), dig.lab = 100, right = FALSE),
-  decada = as.numeric(str_extract(decada_label, "[0-9]+"))
-)
+    data = dmy(data),
+    ano = year(data),
+    decada_label = cut(ano, breaks = seq(1910, 2020, by = 10), dig.lab = 100, right = FALSE),
+    decada = as.numeric(str_extract(decada_label, "[0-9]+"))
+  )
 
 summary(clima)
-```
 
-```{r}
+
+
 ggplot(clima, aes(t_max)) +
   geom_density() +
   labs(
@@ -47,9 +23,9 @@ ggplot(clima, aes(t_max)) +
     y = "Densidade"
   ) +
   theme_bw()
-```
 
-```{r}
+
+
 ggplot(clima, aes(t_max, group = ano, fill = ano)) +
   geom_density(alpha = 0.4) +
   labs(
@@ -59,10 +35,10 @@ ggplot(clima, aes(t_max, group = ano, fill = ano)) +
   ) +
   scale_fill_viridis_c() +
   theme_bw()
-```
 
 
-```{r}
+
+
 ggplot(clima, aes(t_max)) +
   geom_density() +
   facet_wrap(~ano) +
@@ -71,9 +47,9 @@ ggplot(clima, aes(t_max)) +
     y = "Densidade"
   ) +
   theme_few(base_size = 9)
-```
 
-```{r, fig.width = 10, fig.height = 18}
+
+{r, fig.width = 10, fig.height = 18}
 ggplot(clima, aes(t_med, factor(ano))) +
   geom_joy() +
   labs(
@@ -81,9 +57,9 @@ ggplot(clima, aes(t_med, factor(ano))) +
     y = "Ano"
   ) +
   theme_few(base_size = 9)
-```
 
-```{r}
+
+
 aux_mediana <- clima %>% 
   group_by(decada) %>% 
   summarise(mediana = median(t_med, na.rm = TRUE))
@@ -100,9 +76,9 @@ ggplot(clima, aes(t_med, factor(decada), fill = ..x..)) +
   ) +
   scale_fill_viridis_c() +
   theme_few(base_size = 9)
-```
 
-```{r}
+
+
 clima %>% 
   mutate(mes = month(data)) %>% 
   ggplot(aes(t_med, rev(factor(mes)),  fill = ..x..)) +
@@ -114,9 +90,9 @@ clima %>%
   scale_fill_viridis_c() +
   scale_x_continuous(breaks = seq(0,40, 4)) +
   scale_y_discrete(labels = format(ISOdate(2000, 12:1, 1), "%b"))
-```
 
-```{r}
+
+
 clima %>% 
   mutate(mes = month(data)) %>% 
   ggplot(aes(prec, rev(factor(mes)),  fill = ..x..)) +
@@ -128,9 +104,8 @@ clima %>%
   scale_fill_viridis_c() +
   scale_x_continuous(breaks = seq(0,700, 100)) +
   scale_y_discrete(labels = format(ISOdate(2000, 12:1, 1), "%b"))
-```
 
-```{r, fig.height=8, fig.width=4}
+
 clima_normal <- clima %>%
   filter(!is.na(t_med)) %>% 
   group_by(ano = year(data)) %>%
@@ -141,10 +116,10 @@ clima_normal <- clima %>%
 clima_normal %>% 
   filter(!is.na(media_movel)) %>% 
   ggplot(aes(media_movel, ano)) +
-    geom_path() +
-    geom_point() +
-    scale_y_reverse(breaks = seq(1940, 2017, by = 5))
+  geom_path() +
+  geom_point() +
+  scale_y_reverse(breaks = seq(1940, 2017, by = 5))
 
-```
+
 
 
