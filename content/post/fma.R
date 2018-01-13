a7d077c4ee874c296a71024551b71fab44a2a9d2
italocegatta <italocegatta@gmail.com>
2018-01-13 19:25:29 -0200

    hatch bd

--- content/post/fma.R
+++ content/post/fma.R
@@ -1,56 +1,97 @@
 library(tidyverse)
-library(ggjoy)
+library(ggridges)
 library(lubridate)
 
-f_ppt <- function(ppt) {
-  case_when(
-    ppt < 2.5 ~ 1,
-    ppt >= 2.5 & ppt < 5 ~ 0.7,
-    ppt >= 5 & ppt < 10 ~ 0.4,
-    ppt >= 10 & ppt < 12 ~ 0.2,
-    TRUE ~ NA_real_
-  )
-}
+ppt <- head(dados, n = 20)$ppt
+ur <-  head(dados, n = 20)$ur13
+fma[1] <- 0
 
 fma <- function(ur, ppt) {
   n <- length(ur)
-  fma <- numeric(n)
+  fma <- rep(NA_real_, n)
   
   for (i in seq_len(n)) {
-    f_i <- f_ppt(ppt[i])
-    
-    if (is.na(f_i)) {
+
+    if (i == 1) {
       fma[i] <- 0
-    } else {
-      fma[i] <- fma[i] * f_i + (100 / ur[i]) 
+      next()
     }
+    
+    if (is.na(ur[i - 1])) {
+      fma[i] <- 0
+      next()
+    }
+    
+    fma[i] <- case_when(
+      ppt[i] < 2.5 ~ (100 / ur[i]) + fma[i - 1] * 1 ,
+      ppt[i] >= 2.5 & ppt[i] < 5 ~ (100 / ur[i]) + fma[i - 1] * 0.7,
+      ppt[i] >= 5   & ppt[i] < 10 ~ (100 / ur[i]) + fma[i - 1] * 0.4,
+      ppt[i] >= 10  & ppt[i] < 13 ~ (100 / ur[i]) + fma[i - 1] * 0.2,
+      ppt[i] >= 13 ~ 0
+    )
   }
   
   fma
+}
+
+teste <- function(x, y) {
+  x0 <- lag(x)
+  y0 <- lag(y)
+  
+  z[1] <- 0
+  
+  z <- case_when(
+    x < 2.5 ~ (100 / y) + fma[i - 1] * 1 ,
+    ppt[i] >= 2.5 & ppt[i] < 5 ~ (100 / ur[i]) + fma[i - 1] * 0.7,
+    ppt[i] >= 5   & ppt[i] < 10 ~ (100 / ur[i]) + fma[i - 1] * 0.4,
+    ppt[i] >= 10  & ppt[i] < 13 ~ (100 / ur[i]) + fma[i - 1] * 0.2,
+    ppt[i] >= 13 ~ 0
+  )
+  
+  
+}
+
+
+
+
+fma_classe <- function(fma) {
+  case_when(
+    fma <= 1 ~ 1,
+    fma > 1 & fma <= 3 ~ 2,
+    fma > 3 & fma <= 8 ~ 3,
+    fma > 8 & fma <= 20 ~ 4,
+    fma > 20 ~ 5
+  )
 }
 
 fma2 <- function(ur, ppt) {
   
 }
 
-
-
-
-dados <- read_csv2("https://raw.githubusercontent.com/italocegatta/italocegatta.github.io_source/master/content/dados/clima_pira.csv")
+dados <- read_csv2(
+  "https://raw.githubusercontent.com/italocegatta/italocegatta.github.io_source/master/content/dados/clima_pira.csv",
+  col_types = cols(data = col_date(format = "%d/%m/%Y"))
+)
 
 dados
 
 dados_fma <- dados %>% 
   mutate(
-    data = dmy(data),
-    fma = fma(ur, ppt)
+    fma = fma(ur13, ppt),
+    classe = fma_classe(fma),
+    ano = year(data),
+    mes = month(data)
   )
 
 
+dados_fma  %>% 
+  ggplot(aes(data, classe)) +
+    geom_col() +
+    coord_polar()
+
 dados_fma %>% 
-  mutate(ano = year(data)) %>% 
-  ggplot(aes(ur, ano, group = ano)) +
-  geom_joy() +
+  ggplot(aes(fma, factor(mes))) +
+  geom_density_ridges() +
   theme_bw()
 
 dados_fma %>%
@@ -58,6 +99,16 @@ dados_fma %>%
     ano = year(data),
     doy = yday(data)
   ) %>% 
-  ggplot(aes(doy, ano, fill = ur)) +
+  ggplot(aes(doy, ano, fill = ur13)) +
     geom_tile() +
-    scale_fill_viridis_c()
+    scale_fill_distiller(palette = "Blues")
+
+dados_fma %>%
+  mutate(
+    ano = year(data),
+    doy = yday(data)
+  ) %>% 
+  ggplot(aes(doy, factor(ano), fill = fma)) +
+  geom_tile() +
+  scale_fill_distiller(palette = "GnBu", direction = 1) +
+  theme_bw()
